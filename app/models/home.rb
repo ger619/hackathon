5760e816d077bd3859b4875a1f6e4be2b1e1c182
@ger619 <abolger254@gmail.com>
2026-04-09 19:27:27 +0300

    first commit

---
+++ app/models/home.rb
@@ -0,0 +1,40 @@
+class Home < ApplicationRecord
+
+  has_one_attached :qr_code
+
+  include Rails.application.routes.url_helpers
+
+
+  after_create :generate_qr
+
+  def generate_qr
+    qr_url = url_for(controller: 'homes',
+                     action: 'show',
+                     id:,
+                     only_path: false,
+                     host: 'localhost:3000',
+                     source: 'from_qr')
+    qrcode = RQRCode::QRCode.new(qr_url)
+
+    png = qrcode.as_png(
+      resize_gte_to: false,
+      resize_exactly_to: false,
+      fill: 'white',
+      color: 'black',
+      size: 250,
+      border_modules: 4,
+      module_px_size: 6,
+      file: nil # path to write
+    )
+    image_name = SecureRandom.hex
+    File.binwrite("tmp/#{image_name}.png", png.to_s)
+
+    blob = ActiveStorage::Blob.create_and_upload!(
+      io: File.open("tmp/#{image_name}.png"),
+      filename: image_name,
+      content_type: 'png'
+    )
+
+    qr_code.attach(blob)
+  end
+end
